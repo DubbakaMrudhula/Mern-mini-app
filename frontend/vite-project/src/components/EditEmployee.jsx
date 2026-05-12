3de895511a10a2691a3b6464c54b51665ad7bec0
unknown <mrudhu2306@gmail.com>
2026-05-12 21:17:14 +0530

    Update backend URL in frontend

--- frontend/vite-project/src/components/EditEmployee.jsx
+++ frontend/vite-project/src/components/EditEmployee.jsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "https://mern-mini-app-backend1.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://mern-mini-app-2.onrender.com";
 
 function EditEmployee() {
   const {
--- frontend/vite-project/src/components/ListofEmp.jsx
+++ frontend/vite-project/src/components/ListofEmp.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://mern-mini-app-backend1.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://mern-mini-app-2.onrender.com";
 
 function ListOfEmps() {
   const [emps, setEmps] = useState([]);
